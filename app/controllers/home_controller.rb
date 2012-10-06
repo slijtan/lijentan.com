66093377719e4a49363354cb1ae68ec9ee359cf7
Lijen Tan <lijentan@gmail.com>
2012-10-06 12:23:08 -0700

    working parallax

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
     @posts = Post.where(published: true).order('date_published DESC')
 
     @tac_cup = Post.find_by_title("Levitating Sheep - Multiple Backgrounds for Simple Parallax Effect")
+    #@tac_cup = Post.find_by_title("The Tac Cup")
   end
 end

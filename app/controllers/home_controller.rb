cd38b165c8ba6d77b133d81a94986201c2cfc221
Lijen Tan <lijentan@gmail.com>
2012-10-06 13:21:44 -0700

    coffee script is running an alert, updated header background image size and offset

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.where(published: true).order('date_published DESC')
+    @posts = Post.where(published: true).order('date_published DESC').limit(5)
 
     @tac_cup = Post.find_by_title("Levitating Sheep - Multiple Backgrounds for Simple Parallax Effect")
     #@tac_cup = Post.find_by_title("The Tac Cup")

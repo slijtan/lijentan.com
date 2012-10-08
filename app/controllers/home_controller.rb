163ee3f389a9e7af34afb55404275e887ee0d8ba
Lijen Tan <lijentan@gmail.com>
2012-10-08 16:09:45 -0700

    more content updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
     @posts = Post.where(published: true).order('date_published DESC')
 
     @tac_cup = Post.find_by_title("Levitating Sheep - Multiple Backgrounds for Simple Parallax Effect")
-    #@tac_cup = Post.find_by_title("The Tac Cup")
+    #@tac_cup = Post.find_by_title("Summatime in the SFC")
   end
 end

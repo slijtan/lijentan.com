185536f6efdbcca720b8178f82fa846a1c3aecb0
Lijen Tan <lijentan@gmail.com>
2013-03-14 07:19:01 -0700

    adding back ufc

--- db/seeds/svmg.seeds.rb
+++ db/seeds/svmg.seeds.rb
@@ -23,36 +23,36 @@ site.domains.create(domain: "svmg.localhost")
 site.domains.create(domain: "svmg.lijentan.com")
 
 site.site_style.update_attributes(body_font_data: {
-                                    font_code: "<link href='http://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>",
-                                    font_family: "'Muli', sans-serif"
-                                  })
+				    font_code: "<link href='http://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>",
+				    font_family: "'Muli', sans-serif"
+				  })
 
 
 ############# SPLASH PAGE #############
 p = Post.create(title: "SVMG",
-                published: true,
-                show_header: false,
-                site: site,
-                space: "background",
-                height: "100%",
-                display_order: 1)
+		published: true,
+		show_header: false,
+		site: site,
+		space: "background",
+		height: "100%",
+		display_order: 1)
 
 s = Sprite.create(url: 'http://dl.dropbox.com/u/4893047/svmg/logo-svmg.png', style: 'image')
 PostElement.create(post: p, element: s, sequence: 1, position: "fluid-h:50%;fixed-top:250px;", animation_type: "fixed")
 
 tb = TextBox.create(style: 'full-margins',
-                    css: 'text-shadow:none',
-                    text: %{
+		    css: 'text-shadow:none',
+		    text: %{
 <p class="large">Brand Representation - Consulting - Social Media</p>
 },
-                    text_align: "center")
+		    text_align: "center")
 PostElement.create(post: p, element: tb, sequence: 2, position: "fluid-h:50%;fixed-top:450px;", animation_type: "fixed")
 
 tb = TextBox.create(style: 'full-margins',
-                    text: %{
+		    text: %{
 <p class="grayLighter xlarge">&#8595;</p>
 },
-                    text_align: "center")
+		    text_align: "center")
 #PostElement.create(post: p, element: tb, sequence: 3, position: "fixed-bottom:10px;fluid-h:50%", effects: 'fade_out-fade_out-fade_out', media_flag: 1)
 
 s = Sprite.create(url: 'http://dl.dropbox.com/u/4893047/svmg/svmg-arrow.png', style: 'image', width: 21, height: 70)
@@ -60,32 +60,32 @@ PostElement.create(post: p, element: s, sequence: 3, position: "fixed-bottom:15p
 
 ############# Brand Rep #############
 p = Post.create(title: "About SVMG",
-                published: true,
-                show_header: false,
-                site: site,
-                space: 'foreground',
-                height: "100%",
-                depth: 25,
-                display_order: 2)
+		published: true,
+		show_header: false,
+		site: site,
+		space: 'foreground',
+		height: "100%",
+		depth: 25,
+		display_order: 2)
 
 
 s = Sprite.create(url: 'http://dl.dropbox.com/u/4893047/svmg/logo-svmg-outline-large.png', style: 'image', width: 350, height: 350)
 PostElement.create(post: p, element: s, sequence: 1, position: "fluid-v:50%;fixed-grid-right:-1col")
 
 tb = TextBox.create(style: 'single-column-text',
-                    css: 'border-right:2px #999999 solid',
-                    text: '<h1 class="xlarge right" style="margin-top:-27px;height:228px">SVMG</h2>')
+		    css: 'border-right:2px #999999 solid',
+		    text: '<h1 class="xlarge right" style="margin-top:-27px;height:228px">SVMG</h2>')
 #PostElement.create(post: p, element: tb, sequence: 2, position: "fluid-v:50%;fixed-grid-right:-1col")
 
 tb = TextBox.create(style: 'single-column-text',
-                    css: 'padding-left:0px;text-shadow:none',
-                    text: %{<p class="grayLight">Everyday more and more companies are facing the challenges of growing and adapting their business for new market entry and expansion plans.</p>
+		    css: 'padding-left:0px;text-shadow:none',
+		    text: %{<p class="grayLight">Everyday more and more companies are facing the challenges of growing and adapting their business for new market entry and expansion plans.</p>
 <p class="grayLight">Whether it’s leveraging our unique experiences and resources, advising on cultural issues and business protocols, or crafting a customized strategy, SVMG is committed to providing our clients with the tools they need in order to thrive in an ever-evolving globalized marketplace.</p>
 <p class="grayLight"><em>SVMG works with amazing brands to take them to even greater heights.</em></p>})
 PostElement.create(post: p, element: tb, sequence: 3, position: "fluid-v:50%;fixed-grid-left:0col") #, animation_type: 'scan', animation_direction: 'up')
 
 tb = TextBox.create(style: 'full-margins',
-                    text: %{
+		    text: %{
 <p class="gray large center"><em>SVMG works with amazing brands to take them to even greater heights.</em></p>
 })
 #PostElement.create(post: p, element: tb, sequence: 3, position: "fixed-top:220px;fluid-h:50%") #, animation_type: 'scan', animation_direction: 'up')
@@ -96,17 +96,17 @@ s = Sprite.create(url: 'http://dl.dropbox.com/u/4893047/svmg/divider-foreground-
 
 =begin
 p = Post.create(title: "About SVMG",
-                published: true,
-                show_header: false,
-                site: site,
-                space: 'foreground',
-                height: "100%",
-                depth: 26,
-                display_order: 3)
+		published: true,
+		show_header: false,
+		site: site,
+		space: 'foreground',
+		height: "100%",
+		depth: 26,
+		display_order: 3)
 
 tb = TextBox.create(style: 'full-no-margins',
-                    text_align: "center",
-                    text: %{
+		    text_align: "center",
+		    text: %{
 <h1 class="gray" style="font-size:30px">SVMG works with amazing brands to take them to even greater heights.</h1>
 })
 
@@ -119,25 +119,25 @@ s = Sprite.create(url: 'http://dl.dropbox.com/u/4893047/svmg/divider-foreground-
 
 ############# Brand Rep Brands #############
 p = Post.create(title: "MLBAM",
-                published: true,
-                show_header: false,
-                site: site,
-                height: "150%",
-                bg_color: '#003263',
-                space: "background",
-                depth: 3,
-                display_order: 4)
+		published: true,
+		show_header: false,
+		site: site,
+		height: "150%",
+		bg_color: '#003263',
+		space: "background",
+		depth: 3,
+		display_order: 4)
 
 s = Sprite.create(url: 'http://dl.dropbox.com/u/4893047/svmg/logo-mlb.jpg', style: 'cover')
 PostElement.create(post: p, element: s, sequence: 1, position: "fixed-right:0px;fluid-v:50%", animation_type: "fixed-scan", animation_direction: "right")
 
 tb = TextBox.create(style: 'full-margins',
-                    text: "<h1 class='massive'>&#149;</h1>")
+		    text: "<h1 class='massive'>&#149;</h1>")
 PostElement.create(post: p, element: tb, sequence: 2, position: "fixed-bottom:0px", animation_type: "fixed-scan", animation_direction: "right")
 
 tb = TextBox.create(style: 'single-column-text',
-                    css: 'border-right:2px white solid;text-shadow:none',
-                    text: %{
+		    css: 'border-right:2px white solid;text-shadow:none',
+		    text: %{
 <h1 class="huge right" style="margin-top:20px;line-height:90px">MLBAM</h1>
 <p class="large grayLight right">Major League Baseball Advanced Media</p>
 <p class="grayLighter">
@@ -156,45 +156,44 @@ PostElement.create(post: p, element: tb, sequence: 3, position: "fluid-h:20%;flu
 
 
 p = Post.create(title: "Divider",
-                published: true,
-                show_header: false,
-                site: site,
-                space: "foreground",
-                height: "182px",
-                display_order: 5)
+		published: true,
+		show_header: false,
+		site: site,
+		space: "foreground",
+		height: "182px",
+		display_order: 5)
 
 tb = TextBox.create(style: 'single-line-text',
-                    css: 'text-shadow:none',
-                    text: %{
+		    css: 'text-shadow:none',
+		    text: %{
 <p class="right" style="font-size:22px;line-height:28px"><em>&ldquo;With such a passionate following for baseball in Taiwan, it was extremely important that MLBAM find the right partner to advise us on the ground. From deal sourcing to strategic planning, SVMG has exceeded our loftiest expectations.&rdquo;</em></p>
 })
 PostElement.create(post: p, element: tb, sequence: 2, position: "fixed-top:30px;fluid-h:50%") #, animation_type: 'scan', animation_direction: 'right')
 
 tb = TextBox.create(style: 'full-margins',
-                    css: 'text-shadow:none',
-                    text: %{
+		    css: 'text-shadow:none',
+		    text: %{
 <p class="gray right" style="font-size:20px">- Alex Pigeon, Vice President International</p>
 })
 PostElement.create(post: p, element: tb, sequence: 3, position: "fixed-bottom:30px;fluid-h:50%") #, animation_type: 'scan', animation_direction: 'left')
 
 
 tb = TextBox.create(style: 'single-line-text',
-                    text: %{
+		    text: %{
 <h2 class="center"><em>&ldquo;With Taiwan having such a rich history of baseball, it only made sense for MLBAM to find a great partner to advise us on the ground. From deal sourcing to strategic planning, SVMG has exceeded our loftiest expectations.&rdquo;</em></h2>
 <h3 class="gray right">- Alex Pigeon, Vice President International</h3>
 })
 #PostElement.create(post: p, element: tb, sequence: 2, position: "fixed-top:35px;fluid-h:50%")
 
-=begin
 p = Post.create(title: "UFC",
-                published: true,
-                show_header: false,
-                site: site,
-                height: "150%",
-                bg_color: '#000',
-                space: 'background',
-                depth: 2,
-                display_order: 9)
+		published: true,
+		show_header: false,
+		site: site,
+		height: "150%",
+		bg_color: '#000',
+		space: 'background',
+		depth: 2,
+		display_order: 9)
 
 #s = Sprite.create(url: 'http://dl.dropbox.com/u/4893047/svmg/ufc-fence.jpg', style: 'cover')
 #PostElement.create(post: p, element: s, sequence: 1, position: "fixed-top:0px", animation_type: "fixed")
@@ -212,8 +211,8 @@ s = Sprite.create(url: 'http://dl.dropbox.com/u/4893047/svmg/ufc-fence-front.png
 PostElement.create(post: p, element: s, sequence: 4, position: "fixed-top:0px;fixed-left:0px")
 
 tb = TextBox.create(style: 'single-column-text',
-                    css: 'border-right:2px white solid;text-shadow:none',
-                    text: %{
+		    css: 'border-right:2px white solid;text-shadow:none',
+		    text: %{
 <h1 class="huge right" style="margin-top:20px;line-height:90px"><img src='http://dl.dropbox.com/u/4893047/svmg/ufc-logo.png' style='height:82px;margin-right:7px' />UFC</h1>
 
 <p class="large grayLight right">Ultimate Fighting Championship</p>
@@ -230,35 +229,35 @@ Because of its dedication and commitment to growing the UFC brand across Asia, S
 PostElement.create(post: p, element: tb, sequence: 5, position: "fluid-h:80%;fluid-v:80%", animation_type: "three-phase", animation_direction: "fixed-fixed-roll")
 
 p = Post.create(title: "Divider",
-                published: true,
-                show_header: false,
-                site: site,
-                space: "foreground",
-                height: "182px",
-                display_order: 10)
+		published: true,
+		show_header: false,
+		site: site,
+		space: "foreground",
+		height: "182px",
+		display_order: 10)
 
 tb = TextBox.create(style: 'single-line-text',
-                    css: 'text-shadow:none',
-                    text: %{
+		    css: 'text-shadow:none',
+		    text: %{
 <p class="right" style="font-size:22px;line-height:28px"><em>&ldquo;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque adipiscing tincidunt nisl in lacinia. Pellentesque habitant morbi tristique senectus et netus et malesuada fames.&rdquo;</em></p>
 <p class="gray right" style="font-size:20px">- Mark Fischer, Managing Director UFC Asia</p>
 })
 
 PostElement.create(post: p, element: tb, sequence: 2, position: "fixed-top:30px;fluid-h:50%")
-=end
+
 
 
 ############# Taiwan Mobile #############
 
 p = Post.create(title: "Taiwan Mobile",
-                published: true,
-                show_header: false,
-                site: site,
-                height: "150%",
-                bg_color: '#ff6f00',
-                space: 'background',
-                depth: 1,
-                display_order: 11)
+		published: true,
+		show_header: false,
+		site: site,
+		height: "150%",
+		bg_color: '#ff6f00',
+		space: 'background',
+		depth: 1,
+		display_order: 11)
 
 s = Sprite.create(url: 'http://dl.dropbox.com/u/4893047/svmg/twm-sky.jpg', style: 'cover')
 PostElement.create(post: p, element: s, sequence: 1, position: "fixed-top:0px", animation_type: "fixed")
@@ -273,7 +272,7 @@ s = Sprite.create(url: 'http://dl.dropbox.com/u/4893047/svmg/twm-balloons2.png',
 PostElement.create(post: p, element: s, sequence: 4, animation_type: "fixed-scan", animation_direction: "down", position: "fixed-left:0px")
 
 s = Sprite.create(url: 'http://dl.dropbox.com/u/4893047/svmg/twm-balloons3.png', style: 'image')
-PostElement.create(post: p, element: s, sequence: 5, position: "fluid-h:20%;fluid-v:15%")
+PostElement.create(post: p, element: s, sequence: 5, position: "fluid-h:80%;fluid-v:15%")
 
 
 =begin
@@ -308,28 +307,28 @@ s = Sprite.create(url: 'http://dl.dropbox.com/u/4893047/svmg/tm-ball3.png', styl
 =end
 
 tb = TextBox.create(style: 'single-column-text',
-                    css: 'border-right:2px white solid;text-shadow:none',
-                    text: %{
+		    css: 'border-right:2px white solid;text-shadow:none',
+		    text: %{
 <h1 class="huge right" style="margin-top:20px;line-height:90px"><img src='http://dl.dropbox.com/u/4893047/svmg/twm-logo.png' style='height:82px;margin-right:7px' />TWM</h1>
 <p class="large grayLight right">Taiwan Mobile</p>
 <p class="grayLighter">Taiwan Mobile has long been one of the most respected companies not only in the telecom industry, but across all of Taiwan. In every field they’ve entered, they’ve been innovators as well as disruptors. This is no different for their most recent undertaking, the myVideo service.</p>
 <p class="grayLighter">It gives us distinct pride to work along side Taiwan Mobile advising on key elements of this market defining service. From content acquisition to technical implementation, SVMG is honored to be a part of this team.</p>
 })
-#PostElement.create(post: p, element: tb, sequence: 7, position: "fixed-grid-right:-1col;fixed-bottom:150px", animation_type: "three-phase", animation_direction: "fixed-fixed-roll")
-PostElement.create(post: p, element: tb, sequence: 5, position: "fluid-h:80%;fluid-v:80%", animation_type: "three-phase", animation_direction: "fixed-fixed-roll")
+PostElement.create(post: p, element: tb, sequence: 7, position: "fixed-grid-right:-1col;fixed-bottom:150px", animation_type: "three-phase", animation_direction: "fixed-fixed-roll")
+#PostElement.create(post: p, element: tb, sequence: 5, position: "fluid-h:80%;fluid-v:80%", animation_type: "three-phase", animation_direction: "fixed-fixed-roll")
 
 
 p = Post.create(title: "Divider",
-                published: true,
-                show_header: false,
-                site: site,
-                space: "foreground",
-                height: "182px",
-                display_order: 12)
+		published: true,
+		show_header: false,
+		site: site,
+		space: "foreground",
+		height: "182px",
+		display_order: 12)
 
 tb = TextBox.create(style: 'single-line-text',
-                    css: 'text-shadow:none',
-                    text: %{
+		    css: 'text-shadow:none',
+		    text: %{
 <p class="right" style="font-size:22px;line-height:28px"><em>&ldquo;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque adipiscing tincidunt nisl in lacinia. Pellentesque habitant morbi tristique senectus et netus et malesuada fames.&rdquo;</em></p>
 <p class="gray right" style="font-size:20px">- Scylla Tsai, Manager of President's Office</p>
 })
@@ -338,17 +337,17 @@ PostElement.create(post: p, element: tb, sequence: 2, position: "fixed-top:30px;
 
 ############# CONTACT  #############
 p = Post.create(title: "Contact",
-                published: true,
-                show_header: false,
-                site: site,
-                space: "background",
-                height: "100%",
-                display_order: 13)
+		published: true,
+		show_header: false,
+		site: site,
+		space: "background",
+		height: "100%",
+		display_order: 13)
 
 tb = TextBox.create(style: 'full-margins',
-                    text_align: "center",
-                    css: 'text-shadow:none',
-                    text: "<p class='large'>We'd love to hear from you.</p>")
+		    text_align: "center",
+		    css: 'text-shadow:none',
+		    text: "<p class='large'>We'd love to hear from you.</p>")
 PostElement.create(post: p, element: tb, sequence: 2, position: "fluid-h:50%;fixed-top:120px;", animation_type: "fixed", effects: 'scroll_fade_in-show-show')
 
 
@@ -356,9 +355,9 @@ s = Sprite.create(url: 'http://dl.dropbox.com/u/4893047/svmg/logo-svmg.png', sty
 PostElement.create(post: p, element: s, sequence: 1, position: "fluid-h:50%;fixed-top:250px;", animation_type: "fixed")
 
 tb = TextBox.create(style: 'full-margins',
-                    text_align: "center",
-                    css: 'text-shadow:none',
-                    text: %{
+		    text_align: "center",
+		    css: 'text-shadow:none',
+		    text: %{
 <p class="large" style="line-height:22px">Press Inqueries: <a href="mailto:press@svmg.tw" style="color:#FFF">press@svmg.tw</a><br />Business Inqueries: <a href="mailto:business@svmg.tw" style="color:#FFF">business@svmg.tw</a></p>
 })
 PostElement.create(post: p, element: tb, sequence: 2, position: "fluid-h:50%;fixed-top:450px;", animation_type: "fixed")
